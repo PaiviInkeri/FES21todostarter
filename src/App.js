004c490b479914589b1e280d43ddcb35283e036b
PaiviInkeri <paivi.i.immonen@gmail.com>
2021-12-19 17:38:04 +0200

    added menu. The date is now displayed but still doesn´t work properly

--- src/App.js
+++ src/App.js
@@ -8,11 +8,32 @@ import {
   MuiPickersUtilsProvider,
 } from '@material-ui/pickers';
 import 'date-fns';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+//import { TabPanel } from '@mui/lab';
 
-function App() {
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+  return (
+    <div
+      role= "tabpanel"
+      hidden= {value !== index}
+      id= {index}
+    >
+      {
+        value === index && (
+          <div>{children}</div>
+        )
+      }
+    </div>
+  )
+}
+
+export default function App() {
   const [todo, setTodo] = useState({desc: '', date: ''});
   const [todos, setTodos] = useState([]);
   const [selectedDate, setSelectedDate] = useState(new Date());
+  const [value, setValue] = useState(0);
 
   const addTodo = (event) => {
     event.preventDefault();
@@ -34,14 +55,31 @@ function App() {
     var dateToString = day + "." + month + "." + year;
 
     //trying to set date in todo, doesn´t work
-    setTodo({...todo, [todo.date]: dateToString});
+    setTodo({...todo, date: dateToString});
     console.log("This is value of todo.date: " + todo.date);
     console.log(dateToString);
     
   }
 
+  const handleTabs = (event, value) => {
+    console.warn(value);
+    setValue(value);
+  }
+
+
+
   return (
     <div className="App">
+      <Tabs value = {value} onChange = {handleTabs}>
+        <Tab label="Home" index={0}/>
+        <Tab label="My todos" index={1}/>
+      </Tabs>
+
+      <TabPanel value={value} index={0}>
+        <h2>Welcome to Todo List!</h2>
+        </TabPanel>
+      <TabPanel value ={value} index={1}>
+      <br />
       <MuiPickersUtilsProvider utils={DateFnsUtils}>
       <DatePicker
         autoOk
@@ -52,8 +90,11 @@ function App() {
         onChange={handleDateChange}
       />
       </MuiPickersUtilsProvider>
+      <br />
+
       <TextField label="description" type="text" name="desc" value={todo.desc} onChange={inputChanged}/>
       <Button variant="contained" color="primary" onClick={addTodo}>Add</Button>
+
       <table><tbody>
       {
       todos.map((todo, index) => 
@@ -63,8 +104,8 @@ function App() {
         </tr>)
       }
       </tbody></table>
+      </TabPanel>
     </div>
   );
 }
 
-export default App;
